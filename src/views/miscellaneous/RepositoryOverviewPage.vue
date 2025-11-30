bd5fe4cb3b925219db9ebf6b4dfafc721c887e9c
Vixoner <vixonerpl@gmail.com>
2025-11-30 19:49:57 +0100

    feat: Commit chart implementation

--- src/views/miscellaneous/RepositoryOverviewPage.vue
+++ src/views/miscellaneous/RepositoryOverviewPage.vue
@@ -1,9 +1,36 @@
 <template>
-  <InDevelopmentLabel />
+  <div style="width: 600px; height: 300px;">
+    <CommitTimelineChart
+      :data="commitData"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
-  import InDevelopmentLabel from '@/components/common/InDevelopmentLabel.vue'
+  import { ref } from 'vue'
+  import CommitTimelineChart from '@/components/visuals/CommitTimelineChart.vue'
+
+  // Przykładowe dane
+  const commitData = ref([
+    { date: '2024-07-01', commits: 65 },
+    { date: '2024-07-08', commits: 145 },
+    { date: '2024-07-15', commits: 210 },
+    { date: '2024-07-22', commits: 190 },
+    { date: '2024-07-29', commits: 250 },
+    { date: '2024-08-05', commits: 180 },
+    { date: '2024-08-12', commits: 120 },
+    { date: '2024-08-19', commits: 135 },
+    { date: '2024-08-26', commits: 145 },
+    { date: '2024-09-02', commits: 125 },
+    { date: '2024-09-09', commits: 110 },
+    { date: '2024-09-16', commits: 220 },
+    { date: '2024-09-23', commits: 185 },
+    { date: '2024-09-30', commits: 175 },
+    { date: '2024-10-07', commits: 165 },
+    { date: '2024-10-14', commits: 190 },
+    { date: '2024-10-21', commits: 210 },
+    { date: '2024-10-28', commits: 155 },
+  ])
 </script>
 
 <style scoped></style>
