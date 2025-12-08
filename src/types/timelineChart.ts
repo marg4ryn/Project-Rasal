6d1507dd42bfb54e644250e6aac53600f18e4756
Vixoner <121562671+Vixoner@users.noreply.github.com>
2025-12-08 21:42:38 +0100

    feat: More flexible timeline chart

---
+++ src/types/timelineChart.ts
@@ -0,0 +1,21 @@
+export enum ChartColor {
+  Blue = 'blue',
+  Green = 'green',
+  Orange = 'orange',
+  Red = 'red',
+  Purple = 'purple',
+  Pink = 'pink'
+}
+
+export interface ChartDataPoint {
+  date: string // yyyy-mm-dd
+  value: number
+}
+
+export interface Dataset {
+  label: string
+  data: ChartDataPoint[]
+  tooltipDesc?: string
+  yAxisID?: 'left' | 'right'
+  color?: ChartColor
+}
